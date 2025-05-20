241b7bad34de6fd4d78a3ba5408456f39e626dca
Vlăduț Pasăre <vladutpasare@Vladuts-MacBook-Air.local>
2025-05-20 22:39:19 +0300

    Project-Setup

---
+++ backend/PriceComparator-Market/src/main/java/com/backend/PriceComparator_Market/PriceComparatorMarketApplication.java
@@ -0,0 +1,13 @@
+package com.backend.PriceComparator_Market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceComparatorMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceComparatorMarketApplication.class, args);
+	}
+
+}
---
+++ backend/PriceComparator-Market/src/test/java/com/backend/PriceComparator_Market/PriceComparatorMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.PriceComparator_Market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceComparatorMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
